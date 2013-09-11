688bfd474d8b88c7980682dcf1ead7d371ac44cb
runup <645112281@qq.com>
2013-09-11 13:23:52 +0800

    cccc

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,8 +4,8 @@ class Comment < ActiveRecord::Base
   default_scope order: 'comments.created_at DESC'
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true,
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
+                    format: { with: VALID_EMAIL_REGEX }
+
   validates :name, presence: true
   validates :content, presence: true
 end
